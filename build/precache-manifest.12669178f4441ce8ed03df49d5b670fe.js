738c35a6640369b7f60141e87b52fa2b6405b2dd
wangb <wangbinyq@gmail.com>
2020-03-08 14:04:28 +0800

    init release

---
+++ build/precache-manifest.12669178f4441ce8ed03df49d5b670fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720866e6351529fdbeab7cb563cd4b78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35bdf00b7ff1111ef3a0",
+    "url": "/static/css/2.fc652433.chunk.css"
+  },
+  {
+    "revision": "dd82d36b00ff80995c77",
+    "url": "/static/css/main.55211392.chunk.css"
+  },
+  {
+    "revision": "35bdf00b7ff1111ef3a0",
+    "url": "/static/js/2.b9fca8f4.chunk.js"
+  },
+  {
+    "revision": "6ab18a732d32d731e458116394e7b90b",
+    "url": "/static/js/2.b9fca8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd82d36b00ff80995c77",
+    "url": "/static/js/main.09271358.chunk.js"
+  },
+  {
+    "revision": "f39e886b16c5ffa1a551",
+    "url": "/static/js/runtime-main.ef747205.js"
+  }
+]);
